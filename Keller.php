b9a2b2ad1895abc36822323e53b0cc9b6f7a7d3c
guhackzn <guhsouza132@gmail.com>
2026-05-05 09:28:22 -0300

    Tela inicial KellerSS Android

---
+++ Keller.php
@@ -0,0 +1,58 @@
+<?php
+echo '
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>KellerSS-Android</title>
+<style>
+body {
+    background-color: black;
+    font-family: monospace;
+    padding: 20px;
+    color: #00ffcc;
+    white-space: pre;
+    line-height: 1.1; /* diminui o espaço entre linhas */
+ }
+
+.cyan { color: #00ffff; }
+.blue { color: #00bfff; }
+.white { color: #ffffff; }
+.red { color: #ff4444; }
+.yellow { color: #ffff00; }
+.green { color: #00ff00; }
+.purple { color: #A020F0; }
+
+.arrow { color: #00ffff; }
+</style>
+</head>
+<body>
+
+<div class="cyan">KellerSS Android <span class="blue">Fucking Cheaters</span>
+<div class="white">discord.gg/allianceoficial
+<pre class="cyan">
+ _  __       _ _            ______ ______                 
+| |/ / ___ _| | | ___ _._|   ____/ ____|
+|   < / _ ` | | |/ _ \   __| |__ \___  \
+|_|\_ \___,_|_|_|\___/_ |   \____/_____/ 
+ 
+<div class="cyan">Coded By: KellerSS</div>
+<pre class="purple">
+ ╔══════════════════════════╗
+ ║      MENU PRINCIPAL      ║
+ ╚══════════════════════════╝
+</pre>
+<div class="white">ADB:<span class="green"> ● Dispositivo conectado
+
+<span class="yellow">[0]</span> <span class="white">Parear Dispositivo</span>
+<span class="green">[1]</span> <span class="white">Escanear FreeFire Normal</span>
+<span class="green">[2]</span> <span class="white">Escanear FreeFire Max</span>
+<span class="green">[3]</span> <span class="white">Salvar Dump</span>
+<span class="red">[S]</span> <span class="white">Sair</span>
+
+ <div class="arrow">▸ Escolha uma das opções acima:</div>
+
+ </body>
+ </html>
+ ';
+ ?>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+// index.php - página inicial do Keller-Android
+include "Keller.php"; // chama o menu hacker
+?>
